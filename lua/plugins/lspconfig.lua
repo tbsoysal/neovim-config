2cf9dcde514a748c07dedd3666c29b12e5d8d645
tbsoysal <berksysl.g@gmail.com>
2025-02-20 00:36:25 +0300

    Add tabs

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "html", "cssls", "eslint", "ts_ls"}
+        ensure_installed = {"lua_ls", "html", "cssls", "eslint", "ts_ls" }
       })
     end
   },
